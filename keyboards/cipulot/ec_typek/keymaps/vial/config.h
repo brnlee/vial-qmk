f04fdc33dc3f96ce5e766008accf49b5237234f5
brnlee <github.holtz@mail.brnlee.com>
2025-07-15 18:09:49 -0400

    Port "cipulot/ec_typek" to Vial

---
+++ keyboards/cipulot/ec_typek/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x00, 0x3A, 0xE4, 0xED, 0x10, 0xB5, 0xEE, 0x7F}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
